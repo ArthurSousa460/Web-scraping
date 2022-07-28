4699a58e4a57914f6b994e3a7602bc9a55573a48
Arthur <78182643+ArthurSousa460@users.noreply.github.com>
2022-07-28 16:38:11 -0300

    Update main.py

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -7,19 +7,19 @@ table = pd.read_excel('Produtos.xlsx')
 
 
 def update_coin_value():
-    #atualiza a cotação da moeda no dia
+    #atualiza a cotação da moeda no dia e insere na tabela
     table.loc[table['Moeda'] == 'Dólar', 'Cotação'] = float(dolar)
     table.loc[table['Moeda'] == 'Euro', 'Cotação'] = float(euro)
     table.loc[table['Moeda'] == 'Ouro', 'Cotação'] = float(gold)
 
 
 def update_purchase_price():
-    #atualiza o preço de compra da table produtos
+    #atualiza o preço de compra da tabela produtos
     table['Preço de Compra'] = table['Preço Original'] * table['Cotação']
 
 
 def update_sale_price():
-    #atualiza o preço de venda
+    #atualiza o preço de venda da tabela
     table['Preço de Venda'] = table['Preço de Compra'] * table['Margem']
 
 
@@ -27,4 +27,5 @@ update_coin_value()
 update_purchase_price()
 update_sale_price()
 
+#exporta a atualização para um novo arquivo
 table.to_excel('Produtos Novo.xlsx', index=False)
